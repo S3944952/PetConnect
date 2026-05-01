811dfc3129e5b353093a3116db0ccde645f4dd5d
mark117e <S394452@student.rmit.edu.au>
2026-05-02 01:20:07 +1000

    Add shared PHP include files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = 'Home | PetConnect';
+require_once 'includes/header.inc';
+require_once 'includes/nav.inc';
+?>
+
+<main class="container my-5">
+  <h1>PetConnect is working</h1>
+  <p>If you can see this page, the header, navigation and footer includes are working.</p>
+</main>
+
+<?php require_once 'includes/footer.inc'; ?>
